02f7fb997e749ed9d0b20fe14a81884827a35932
Moritz Fink <moritz.fink@accenture.com>
2022-05-11 22:30:27 +0200

    Rework text theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:teamutil/pages/home_page.dart';
 
 void main() {
@@ -14,7 +15,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Estimation Tinder',
       theme: ThemeData(
-        fontFamily: 'Georgia',
+        textTheme: GoogleFonts.pacificoTextTheme(
+          Theme.of(context).textTheme,
+        ),
         primarySwatch: Colors.pink,
       ),
       home: const HomePage(),
