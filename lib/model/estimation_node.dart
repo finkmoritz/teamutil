9eb341895c4d3d19a43717776be1a1cbc8431715
Moritz Fink <moritz.fink@accenture.com>
2022-05-11 14:34:22 +0200

    Draft EstimationNode

--- lib/model/estimation_node.dart
+++ lib/model/estimation_node.dart
@@ -1,12 +1,15 @@
 class EstimationNode<T> {
   final String question;
+  int numberOfVisits = 0;
+  int maxVisits;
   final EstimationNode? parent;
   final List<EstimationNode>? children;
   final T? answer;
   final Function<int>(EstimationNode currentNode)? evaluationFunc;
 
   EstimationNode(
-    this.question, {
+    this.question,
+    this.maxVisits, {
     this.parent,
     this.children,
     this.answer,
